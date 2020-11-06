1f34ec54fc8448fabc6a0e8950cf8add85746e87
Brittany <sincerelybr@gmail.com>
2020-11-05 21:58:58 -0500

    adds pokeball when pokemon are loading

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {PokemonCard, PokeBtn} from './Styles';
+import pokeball from './assets/pokebal_img.png'
 
 export default function Pokemon(props){
+
+  const [imgLoaded, setImgLoaded] = useState(false)
 
   const handleClick = e => {
     console.log(props)
      props.handlePokemonClick(props)
   }
 
+  const handleLoad = () => {
+    setImgLoaded(true)
+  }
+
   return(
     <PokemonCard>
       <h2> {props.name} </h2>
-      <img src={props.spriteUrl} />
+      <img src={props.spriteUrl} 
+      height={90} 
+      width={90} 
+      style={{visibility: imgLoaded ? 'visible' : 'hidden'}} 
+      onLoad={handleLoad}/>
+      <img src={pokeball} 
+      height={90} 
+      width={90} 
+      style={{visibility: !imgLoaded ? 'visible' : 'hidden'}} />
       <PokeBtn onClick={handleClick}>{props.buttonLabel}</PokeBtn>
     </PokemonCard>
   )
